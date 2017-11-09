323471ba3c7a2897db60668a0f485dc629852764
ennvita <ennvita@users.noreply.github.com>
2017-11-09 15:45:06 -0700

    Update time.py

--- cogs/time.py
+++ cogs/time.py
@@ -7,8 +7,8 @@ from discord.ext import commands
 
 from discord import embeds
 
-@commands.group(pass_context=True):
-async def time(self, ctx)
+@commands.group(pass_context=True)
+async def time(self, ctx):
   if ctx.invoked_subcommand is None:
     async with ctx.typing():
       await asyncio.sleep(.5)
