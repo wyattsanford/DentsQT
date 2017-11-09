0f3d2b227b749625dfea8e2ed4819512758cb6de
ennvita <ennvita@users.noreply.github.com>
2017-11-09 16:55:18 -0700

    Update time.py

--- cogs/time.py
+++ cogs/time.py
@@ -8,14 +8,14 @@ from discord.ext import commands
 from discord import embeds
 class Time:
 	def __init__(self, bot):
-		bot = self.bot
+		self.bot = bot
 
 	@commands.command(pass_context=True)
 	async def time(self, message):
 #		if ctx.invoked_subcommand is None:
 #		async with ctx.typing():
 		await asyncio.sleep(.5)
-		return await bot.say(embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string()))
+		return await self.bot.say(embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string()))
 
 def setup(bot):
   bot.add_cog(Time(bot))
