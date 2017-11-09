c113e8269307cf86a8a2cd9cd89a39dfc79d6701
ennvita <ennvita@users.noreply.github.com>
2017-11-09 14:59:55 -0700

    Update time.py

--- cogs/time.py
+++ cogs/time.py
@@ -7,4 +7,9 @@ from discord.ext import commands
 
 from discord import embeds
 
+@commands.group(pass_context=True)
 async def time()
+  if ctx.invoked_subcommand is None:
+    async with ctx.typing():
+      await asyncio.sleep(.5)
+      return await bot.say(embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string())
