a867b70dfb4578ae0a10de0773cd892d3d28952f
ennvita <eldon.vita85@gmail.com>
2017-11-09 18:29:36 -0700

    added user current time

--- cogs/time.py
+++ cogs/time.py
@@ -6,16 +6,22 @@ import pendulum
 from discord.ext import commands
 
 from discord import embeds
-class Time:
-	def __init__(self, bot):
-		self.bot = bot
 
-	@commands.command(pass_context=True)
-	async def time(self, message):
-#		if ctx.invoked_subcommand is None:
-#		async with ctx.typing():
-		await asyncio.sleep(.5)
-		return await self.bot.say(embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string()))
+
+class Time:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def time(self, message):
+        #		if ctx.invoked_subcommand is None:
+        #		async with ctx.typing():
+        await asyncio.sleep(.5)
+        return await self.bot.say(
+            embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string()))
+        return await self.bot.say(embed=embeds.Embed(title='Current USTZ (West Coast:',
+                                                     descrpition=pendulum.timezone('US/Pacific').to_datetime_string()))
+
 
 def setup(bot):
-  bot.add_cog(Time(bot))
+    bot.add_cog(Time(bot))
