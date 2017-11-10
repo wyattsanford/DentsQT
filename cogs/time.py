6b2f1003f20913e2695014cd6a3c9b473e9a139b
ennvita <eldon.vita85@gmail.com>
2017-11-10 11:16:13 -0700

    fixed indentation

--- cogs/time.py
+++ cogs/time.py
@@ -14,13 +14,18 @@ class Time:
 
     @commands.group(pass_context=True)
     async def time(self, ctx):
+        """
+        Gives the current EVE time.
+
+        Time PST gives current time in USTZ: West Coast
+        """
         if ctx.invoked_subcommand is None:
             await asyncio.sleep(.5)
             return await self.bot.say(
                 embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string()))
 
     @time.command()
-    async def pst(self, time):
+    async def pst(self):
         await asyncio.sleep(.5)
         return await self.bot.say(embed=embeds.Embed(title='Current USTZ (West Coast:)',
                                                      descrpition=pendulum.timezone('US/Pacific').to_datetime_string()))
