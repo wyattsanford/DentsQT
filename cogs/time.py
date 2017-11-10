0c7e40423114a0cbb8076c2e6736afd107450649
root <wyattsanford@gmail.com>
2017-11-10 17:43:58 +0000

    ddd

--- cogs/time.py
+++ cogs/time.py
@@ -9,24 +9,22 @@ from discord import embeds
 
 
 class Time:
-    def __init__(self, bot):
-        self.bot = bot
+	def __init__(self, bot):
+		self.bot = bot
 
-    @commands.group(pass_context=True)
-    async def time(self, ctx):
-        #		if ctx.invoked_subcommand is None:
-        #		async with ctx.typing():
-        if ctx.invoked_subcommand is None:
-            await asyncio.sleep(.5)
-            return await self.bot.say(
+	@commands.group(pass_context=True)
+	async def time(self, ctx):
+		 if ctx.invoked_subcommand is None:
+			await asyncio.sleep(.5)
+			return await self.bot.say(
             embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string()))
 
-    @time.command()
-    async def pst(self, ctx, time):
-        await asyncio.sleep(.5)
-        return self.bot.say(embed=embeds.Embed(title='Current USTZ (West Coast:)',
+	@time.command()
+	async def pst(self, ctx, *, time):
+		await asyncio.sleep(.5)
+		return await self.bot.say(embed=embeds.Embed(title='Current USTZ (West Coast:)',
                                                      descrpition=pendulum.timezone('PST').to_datetime_string()))
 
 
 def setup(bot):
-    bot.add_cog(Time(bot))
+	bot.add_cog(Time(bot))
