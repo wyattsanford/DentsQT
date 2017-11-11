2760ab72fd06969afa45175812debcd57857920e
ennvita <eldon.vita85@gmail.com>
2017-11-10 22:51:12 -0700

    rollback

--- bot.py
+++ bot.py
@@ -11,17 +11,17 @@ import random
 import asyncio
 
 try:
-    import uvloop
+	import uvloop
 except ImportError:
-    pass
+	pass
 else:
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+	asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 try:
-    import config
+	import config
 except ImportError:
-    print('Config not found, please ensure you have set it up correctly')
-    sys.exit(1)
+	print('Config not found, please ensure you have set it up correctly')
+	sys.exit(1)
 
 description = """This is a barebones bot to learn discord.py"""
 bot = commands.Bot(command_prefix='d.', description=description)
@@ -29,63 +29,56 @@ bot = commands.Bot(command_prefix='d.', description=description)
 print(discord.__version__)
 
 initial_cogs = (
-    'cogs.time',
+	'cogs.time',
 )
 
 for cog in initial_cogs:
-    try:
-        bot.load_extension(cog)
-        print('loaded cog {}'.format(cog), file=sys.stderr)
-    except Exception as e:
-        print('Failed to load cog {}'.format(cog), file=sys.stderr)
-        traceback.print_exc()
-
+	try:
+		bot.load_extension(cog)
+		print('loaded cog {}'.format(cog), file=sys.stderr)
+	except Exception as e:
+		print('Failed to load cog {}'.format(cog), file=sys.stderr)
+		traceback.print_exc()
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-    print('Ready')
-
+	print('Logged in as')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('------')
+	print('Ready')
 
 async def process_commands(bot, message):
-    ctx = await bot.get_context(message, cls=context.Context)
-
-    if ctx.command is None:
-        return
-    await bot.invoke(ctx)
-
+	ctx = await bot.get_context(message, cls=context.Context)
+	
+	if ctx.command is None:
+		return
+	await bot.invoke(ctx)
 
 @bot.event
 async def on_message(message):
-    if message.author.bot:
-        return
-    await bot.process_commands(message)
-
-
+	if message.author.bot:
+		return
+	await bot.process_commands(message)
+	
 def run(self):
-    super().run(config.token, reconnect=True)
-
+	super().run(config.token, reconnect=True)
 
 @bot.command()
-async def roll(dice: str):
-    """Rolls a dice, format = NdN"""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await bot.say('Format must be NdN')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.say(result)
-
+async def roll(dice : str):
+	"""Rolls a dice, format = NdN"""
+	try:
+		rolls, limit = map(int, dice.split('d'))
+	except Exception:
+		await bot.say('Format must be NdN')
+		return
+	
+	result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+	await bot.say(result)
 
 @bot.command()
 async def shutdown():
-    await bot.say('Thx for fleet, Tau!')
-    quit('Thx for fleet, Tau!')
-
+	bot.say('Thx for fleet, Tau!')
+	sys.exit('Thx for fleet, Tau!')
 
 bot.run(config.token)
--- cogs/time.py
+++ cogs/time.py
@@ -12,23 +12,16 @@ class Time:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(pass_context=True)
-    async def time(self, ctx):
-        """
-        Gives the current EVE time.
-
-        Time PST gives current time in USTZ: West Coast
-        """
-        if ctx.invoked_subcommand is None:
-            await asyncio.sleep(.5)
-            return await self.bot.say(
-                embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string()))
-
-    @time.command()
-    async def pst(self):
+    @commands.command(pass_context=True)
+    async def time(self, message):
+        #		if ctx.invoked_subcommand is None:
+        #		async with ctx.typing():
         await asyncio.sleep(.5)
-        return await self.bot.say(embed=embeds.Embed(title='Current USTZ (West Coast):',
-                                                     descrpition=pendulum.now(tz='US/Pacific').tz)
+        return await self.bot.say(
+            embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string()))
+#        return await self.bot.say(embed=embeds.Embed(title='Current USTZ (West Coast:',
+#                                                     descrpition=pendulum.timezone('US/Pacific').to_datetime_string()))
+
 
 def setup(bot):
-            bot.add_cog(Time(bot))
+    bot.add_cog(Time(bot))
