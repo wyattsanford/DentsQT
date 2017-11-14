a7521163a3ce39e0331c2a696f3ba3579d925c17
ennvita <eldon.vita85@gmail.com>
2017-11-13 20:30:54 -0700

    added uptime / semi-add join

--- cogs/time.py
+++ cogs/time.py
@@ -13,17 +13,17 @@ class Time:
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def time(self, message):
-        #		if ctx.invoked_subcommand is None:
-        #		async with ctx.typing():
-        await asyncio.sleep(.5)
-        return await self.bot.say(
-            embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string()))
+
+        async def time(self, message):
+            if ctx.invoked_subcommand is None:
+                async with ctx.typing():
+                    await asyncio.sleep(.5)
+                    return await self.bot.say(embed=embeds.Embed(title='Current Eve Time:', description=pendulum.utcnow().to_datetime_string()))
 
     @time.command()
     async def pst(self, message):
         await asyncio.sleep(.5)
-        return await self.bot.say(embed=embeds.Embed(title='Current USTZ (West Coast):', descrpition=pendulum.timezone('US/Pacific').to_datetime_string()))
+        return await self.bot.say(embed=embeds.Embed(title='Current USTZ (West Coast):', descrpition=pendulum.now('US/Pacific').to_datetime_string()))
 
 
 def setup(bot):
